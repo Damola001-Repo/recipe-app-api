9c9c20fcd09c97087e91464d73ca7c52ad86aafc
damola <damolabalogun79@gmail.com>
2025-09-20 13:41:12 +0100

    Commiting because i need to compare

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt!3kn(s0as(a&$jgt$3@cl(l2383cvr&q!!1&b^@yh0%&n!y='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
